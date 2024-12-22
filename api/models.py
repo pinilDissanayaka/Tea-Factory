b23a59c7702a07d7b55f09dbce4da7fde8fc4720
pinilDissanayaka <pinildissanayaka@gmail.com>
2024-12-22 14:44:52 +0530

    refactor

--- api/models.py
+++ api/models.py
@@ -3,14 +3,13 @@ from uuid import uuid4
 
 
 class TeaLeaves(models.Model):
-    id=models.UUIDField(primary_key=True, default=uuid4)
+    id = models.UUIDField(primary_key=True, default=uuid4)
     provider_name = models.CharField(max_length=120)
-    collected_weight=models.DecimalField(max_digits=100, decimal_places=3)
-    any_note=models.TextField(null=True, blank=True)
+    collected_weight = models.DecimalField(max_digits=100, decimal_places=3)
+    any_note = models.TextField(null=True, blank=True)
     collected_date = models.DateTimeField(auto_now_add=True)
 
-
     def __str__(self):
-        return (f"{self.provider_name} gives {self.collected_weight} at {self.collected_date}")
+        return f"{self.provider_name} gives {self.collected_weight} at {self.collected_date}"
     
     
--- api/views.py
+++ api/views.py
@@ -7,21 +7,52 @@ from rest_framework import status
 
 @api_view(['GET', "POST"])
 def all_records(request):
+    """
+    Handle GET and POST requests for all leaves records.
+
+    If the request method is GET, retrieve all records from the database
+    and serialize the data. Return a JsonResponse with the serialized data.
+
+    If the request method is POST, serialize the data from the request body
+    and validate the data. If the data is valid, save the data to the database
+    and return a JsonResponse with the serialized data and a status code of 201
+    Created. If the data is not valid, return a JsonResponse with an error status.
+
+    Parameters
+    ----------
+    request : HttpRequest
+        The request object.
+
+    Returns
+    -------
+    JsonResponse
+        The response object.
+    """
     if request.method=="GET":
+        # Retrieve all records from the database
         data = TeaLeaves.objects.all()
 
+        # Serialize the data
         serialized_data=TeaLeavesSerializer(data, many=True)
 
+        # Return a JsonResponse with the serialized data
         return JsonResponse({"leaves":serialized_data.data}, 
                             safe=False)
 
     if request.method=="POST":
+        # Serialize the data from the request body
         serialized_data=TeaLeavesSerializer(data=request.data)
+
+        # Validate the data
         if serialized_data.is_valid():
+            # Save the data to the database
             serialized_data.save()
+
+            # Return a JsonResponse with the serialized data and a status code of 201 Created
             return JsonResponse({serialized_data},
                                 status=status.HTTP_201_CREATED)
         else:
+            # Return a JsonResponse with an error status
             return JsonResponse({"status": "error"})
 
 
