9857ccb615826c95346510545a68f480c826e03c
pinilDissanayaka <pinildissanayaka@gmail.com>
2024-12-31 21:56:48 +0530

    change the model

--- api/models.py
+++ api/models.py
@@ -5,5 +5,5 @@ class TeaLeaves(models.Model):
     quality=models.CharField(max_length=100)
     quantity=models.FloatField(max_length=100)
     description=models.TextField(max_length=100, blank=True, null=True)
-    collectored_at=models.DateTimeField(auto_now_add=True)
+    collector_at=models.DateTimeField(auto_now_add=True)
     collector_name=models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name="collector_name")
