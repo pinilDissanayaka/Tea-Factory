3cb13c7c84fab411060c4640a8dce3897fb9ccef
pinilDissanayaka <pinildissanayaka@gmail.com>
2025-02-08 13:21:35 +0530

    add db url

--- database.py
+++ database.py
@@ -6,9 +6,7 @@ import os
 
 load_dotenv(find_dotenv())
 
-DATABASE_URL='postgresql://neondb_owner:npg_JNOti13AfaTV@ep-odd-water-a1vdqbo9-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require'
-
-engine=create_engine(url=DATABASE_URL)
+engine=create_engine(url=os.getenv('DATABASE_URL'))
 
 Base=declarative_base()
 
